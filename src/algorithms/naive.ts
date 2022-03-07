0026cb236d9b61156f94822cbfbe6042f2ddf9c6
Gabriel Bitencourt <gabrielbitencourt25@hotmail.com>
2022-03-07 19:20:51 -0300

    nao é necessário probabilidade no algoritmo ingenuo

--- src/algorithms/naive.ts
+++ src/algorithms/naive.ts
@@ -3,7 +3,7 @@ import { Correctness } from "../models/correctness";
 import { IGuess } from "../models/guess";
 import { IGuesser } from "../models/guesser";
 import { dictionary } from "../static/dictionary";
-import { frequencies, total } from "../static/frequencies";
+import { frequencies } from "../static/frequencies";
 
 type Candidate = { word: string; count: number; };
 
